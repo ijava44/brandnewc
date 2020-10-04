c5c8be3d3b61cfbbcd39cfadadce230890944d3a
Ivan Ecb Schulz <ischulz105482@gmail.com>
2020-10-04 11:23:55 -0500

    initial commit

---
+++ four.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+
+{
+    printf("\n This text for sale!!!\n\n");
+   printf ("%f",  500000.45/345.76  );
+    printf ("\n\n");
+    printf ("Hello World");
+    printf("\n\n");
+    printf ("Hello World");
+    printf("\n\n");
+ 
+    printf ("Hello World");
+    printf("\n\n");
+ 
+    printf ("Hello World");
+    printf("\n\n");
+ 
+    printf ("Hello World");
+    printf("\n\n");
+ 
+    printf ("Hello World");
+    printf("\n\n");
+ 
+            }
+
---
+++ three.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+
+{
+    printf("\n This text for sale!!!\n\n");
+
+            }
+
+
