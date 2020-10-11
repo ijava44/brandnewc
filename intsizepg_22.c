6ab12896683ad7693181aeea3d36f8904ce32c10
Ivan Schulz <ischulz105482@outlook.com>
2020-10-10 22:01:24 -0500

    The update from the 32 hp pavilion

--- intsizepg_22.c
+++ intsizepg_22.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main()
 
-    }
+{ 
 
 
+		printf("\n\n");
+		printf("%i", (3 + 3) / 2); 
 
+		printf("\n\n");
+		printf("this is my goddamn name!!! Deal with it!!!  OK!!");
+		printf("\n\n\n");
+		
     }
 
